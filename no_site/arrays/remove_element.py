364cb1d46e491127351938a911821dbd48cc0d71
Francisco Castanho <castanho.jfc@gmail.com>
2021-01-01 19:10:02 +0000

    Add yapf dependency

---
+++ no_site/arrays/remove_element.py
@@ -0,0 +1,13 @@
+"""
+Remove a given element from an array and shift the remaining elements.
+"""
+
+
+def remove_element(array, position):
+    print(array)
+
+
+if __name__ == "__main__":
+    array = [2, 3, 5, 6]
+
+    remove_element(input, 0)
