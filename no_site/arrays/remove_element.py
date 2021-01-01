a478d3bdfb110bde34c73cec7b46f83a806d7bfb
Francisco Castanho <castanho.jfc@gmail.com>
2021-01-01 19:33:25 +0000

    Add remove element

--- no_site/arrays/remove_element.py
+++ no_site/arrays/remove_element.py
@@ -1,13 +1,35 @@
 """
+Challenge:
 Remove a given element from an array and shift the remaining elements.
+
+Solution:
+(1) Create an empty array.
+(2) Go through each element of the array.
+    (a) If the current element index is different from the provided position:
+    copy the element.
+
+Runtimes:
+Space: O(1)
+Time: O(N)
 """
 
 
-def remove_element(array, position):
-    print(array)
+def remove_element(array: list, position: int):
+    """
+    Removes the element in the provided position from the list.
+    """
+    result: list = []
+
+    for index, element in enumerate(array):
+        if index != position:
+            result.append(element)
+
+    return result
 
 
 if __name__ == "__main__":
-    array = [2, 3, 5, 6]
+    input = [2, 3, 5, 6]
 
-    remove_element(input, 0)
+    result = remove_element(input, 1)
+
+    print(result)
